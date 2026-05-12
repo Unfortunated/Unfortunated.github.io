e6e2292ccaede64ce6c580e33ebb348bce39f444
Unfortunated <thanyathornwiengperm@gmail.com>
2026-05-12 16:12:13 +0700

    feat: landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,121 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import Header from './component/header'
+import Footer from './component/footer'
+import roundphoto from './assets/roundphoto.svg'
+import java from './assets/java.svg'
+import js from './assets/js.svg'
+import py from './assets/py.svg'
+import terminal from './assets/terminal.svg'
+import ts from './assets/ts.svg'
+import figma from './assets/figma.svg'
+import jest from './assets/jest.svg'
+import playwright from './assets/playwright.svg'
+import postman from './assets/postman.svg'
+import react from './assets/react.svg'
+import List from './component/list'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+    <div className='flex h-screen w-full flex-col justify-between'>
+      <Header />
+      <div className='max-w-8xl mx-auto flex w-full flex-col px-10'>
+        <div className='flex flex-row items-center justify-center gap-20'>
+          <img src={roundphoto} />
+          <div className='flex flex-col text-xl'>
+            <span>
+              Hello, I am Paopao, an undergraduate from Chulalongkorn
+              University.
+            </span>
+            <span>
+              I am interested in software testing and software engineering.
+            </span>
+            <span>
+              On my free time, I like to play games, watch movies and animes.
+            </span>
+          </div>
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
+        <div className='flex flex-row justify-between px-60'>
+          <div className='flex flex-col items-center gap-5 text-4xl'>
+            <List
+              title='Programming Languages'
+              imgSrc={[ts, py, java, js, terminal]}
+            />
+          </div>
+          <div className='flex flex-col items-center gap-5 text-4xl'>
+            <List
+              title='Technologies'
+              imgSrc={[react, playwright, jest, postman, figma]}
+            />
+          </div>
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+      </div>
+      <Footer />
+    </div>
   )
 }
 
