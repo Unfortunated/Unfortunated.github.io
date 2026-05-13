ee6c931e453a8ad29b92e5fa794cc417b73cdc85
Unfortunated <120785157+Unfortunated@users.noreply.github.com>
2026-05-13 12:42:53 +0700

    feat: routing setup (#9)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import './index.css'
 import App from './App.tsx'
+import Education from './page/education.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/education' element={<Education />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 )
