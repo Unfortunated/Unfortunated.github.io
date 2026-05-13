1cb4c2226dbea510b7c4a417a923f69dbb7cadca
Unfortunated <120785157+Unfortunated@users.noreply.github.com>
2026-05-13 15:15:46 +0700

    setup: routes to all page (#11)

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -1,9 +1,11 @@
 import Button from './button'
 import { Menu } from 'lucide-react'
 import { useState } from 'react'
+import { useNavigate } from 'react-router'
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false)
+  const nav = useNavigate()
   return (
     <div
       data-testid='header'
@@ -11,7 +13,13 @@ const Header = () => {
     >
       <div className='flex flex-row justify-between'>
         <div className='text-xl font-bold md:text-4xl'>
-          <Button onClick={() => {}}>Thanyathorn Wiengperm</Button>
+          <Button
+            onClick={() => {
+              nav('/')
+            }}
+          >
+            Thanyathorn Wiengperm
+          </Button>
         </div>
         <div className='flex flex-row md:hidden'>
           <Button
@@ -22,16 +30,52 @@ const Header = () => {
           />
         </div>
         <div className='hidden flex-row gap-10 text-2xl font-bold md:flex'>
-          <Button onClick={() => {}}>Education</Button>
-          <Button onClick={() => {}}>Experience</Button>
-          <Button onClick={() => {}}>Projects</Button>
+          <Button
+            onClick={() => {
+              nav('/education')
+            }}
+          >
+            Education
+          </Button>
+          <Button
+            onClick={() => {
+              nav('/experience')
+            }}
+          >
+            Experience
+          </Button>
+          <Button
+            onClick={() => {
+              nav('/projects')
+            }}
+          >
+            Projects
+          </Button>
         </div>
       </div>
       {menuOpen && (
         <div className='z-2 mt-4 flex flex-col gap-2 font-bold md:hidden'>
-          <Button onClick={() => {}}>Education</Button>
-          <Button onClick={() => {}}>Experience</Button>
-          <Button onClick={() => {}}>Projects</Button>
+          <Button
+            onClick={() => {
+              nav('/education')
+            }}
+          >
+            Education
+          </Button>
+          <Button
+            onClick={() => {
+              nav('/experience')
+            }}
+          >
+            Experience
+          </Button>
+          <Button
+            onClick={() => {
+              nav('/projects')
+            }}
+          >
+            Projects
+          </Button>
         </div>
       )}
     </div>
