231e70d8a287efcd30a27d14b182ec96b8aa045d
Unfortunated <thanyathornwiengperm@gmail.com>
2026-05-13 17:16:52 +0700

    feat:high school

--- src/page/education/highschool.tsx
+++ src/page/education/highschool.tsx
@@ -1,12 +1,42 @@
 import Header from '../../component/header'
 import Footer from '../../component/footer'
+import Button from '../../component/button'
+import { useNavigate } from 'react-router'
+import { BookOpenText } from 'lucide-react'
+import Textcard from '../../component/textcard'
+import highschool from '../../assets/highschool.svg'
 
 function Highschool() {
+  const nav = useNavigate()
   return (
     <div className='flex min-h-screen w-full flex-col'>
       <Header />
-      <div className='max-w-8xl mx-auto my-auto flex w-full flex-col justify-center gap-5 px-5 md:flex-row md:px-10 2xl:gap-120'>
-        hello
+      <div className='my-auto flex flex-col items-center justify-center gap-10'>
+        <div className='flex flex-row items-center gap-10 md:gap-20'>
+          <div className='text-lg font-bold md:text-xl'>High School</div>
+          <Button
+            onClick={() => {
+              nav('/education')
+            }}
+          >
+            <div className='ring-accent rounded-full p-4 ring-4'>
+              <BookOpenText className='stroke-accent h-4 w-4 md:h-9 md:w-9' />
+            </div>
+          </Button>
+          <div className='text-lg font-bold md:text-xl'>Graduated 2022</div>
+        </div>
+        <div className='flex flex-col items-center gap-5 md:flex-row md:items-start'>
+          <img src={highschool} className='3xl:w-100 w-48' />
+          <Textcard
+            time='2019 - 2022'
+            title='Nawaminthrachinuthit Triamudomsuksanomklao School (NTUN)'
+            content={[
+              'Studied in Science and Math Elective in English Program',
+              'Elected as class representative consecutively for 3 years.',
+              'Participated in English debate club consecutively for 3 years.',
+            ]}
+          />
+        </div>
       </div>
       <Footer />
     </div>
