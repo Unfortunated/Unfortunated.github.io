a62fb4ea5ba98fc5e514b7fd1f751eb2136aa211
Unfortunated <120785157+Unfortunated@users.noreply.github.com>
2026-05-14 13:21:16 +0700

    feat: employment (#16)

--- src/page/experience/employment.tsx
+++ src/page/experience/employment.tsx
@@ -1,12 +1,28 @@
 import Header from '../../component/header'
 import Footer from '../../component/footer'
+import Textcard from '../../component/textcard'
+import { employment } from '../../data/experience'
 
 function Employment() {
+  const data = employment
   return (
     <div className='flex min-h-screen w-full flex-col'>
       <Header />
-      <div className='max-w-8xl mx-auto my-auto flex w-full flex-col justify-center gap-5 px-5 md:flex-row md:px-10 2xl:gap-120'>
-        upapa
+      <div className='my-10 flex flex-col items-center justify-center gap-10'>
+        <div className='flex w-full max-w-5xl flex-col gap-10 px-4'>
+          <div className='flex flex-col-reverse items-center gap-5 md:flex-row md:items-start'>
+            <Textcard
+              time={data[0].time}
+              title={data[0].role}
+              event={data[0].org}
+              content={data[0].content}
+            />
+            <img
+              src={data[0].img}
+              className='3xl:w-100 w-48 shrink-0 object-cover'
+            />
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
